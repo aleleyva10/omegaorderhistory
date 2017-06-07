e4ab1f27774817f58a85267e3e87a364e964074a
Peter Wildberger <pete.wildberger@gmail.com>
2017-06-07 15:24:36 -0500

    sums

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,4 +2,92 @@ $(onReady);
 
 function onReady (){
   console.log('JS/JQ is sourced');
+  getList();
+$('#customer').on('click', 'button', function(){
+  var id = $(this).data('id');
+  console.log(id);
+  getOrders(id);
+});
+
+
+
+}
+
+
+function getList(){
+  $.ajax({
+    type: 'GET',
+    url: '/customers',
+    success: function(response) {
+      console.log('meow', response);
+      displayCust(response);
+    }
+  });
+}
+
+function displayCust(response){
+  $('#customer').empty();
+for (var i = 0; i < response.length; i++) {
+  var custDiv = '<div class="customer">';
+    custDiv += '<p>';
+    custDiv += '<button data-id="' + response[i].id + '">INFO</button>  ';
+    custDiv += response[i].first_name + ' ' + response[i].last_name;
+    custDiv += '</p>';
+    custDiv += '</div>';
+    $('#customer').append(custDiv);
+}
+
+
+}
+function getOrders(id){
+  $.ajax({
+    type: 'GET',
+    url: '/orders/'+ id,
+    success: function(response) {
+      console.log('meow', response);
+      displayOrders(response);
+    }
+  });
+}
+
+function displayOrders(response){
+  $('#myTable').find("tr:gt(0)").remove();
+    var itemQuan = [];
+    var totalSum = [];
+
+for (var i = 0; i < response.length; i++) {
+var itemSum = 0;
+var price = response[i].unit_price;
+
+  var tableRow = '<tr>';
+        tableRow += '<td>' + response[i].id + '</td>';
+        tableRow += '<td>' + response[i].description + '</td>';
+        tableRow += '<td>' + price + '</td>';
+        tableRow += '<td>' + response[i].quantity + '</td>';
+        itemSum = response[i].unit_price * response[i].quantity;
+        totalSum.push(itemSum);
+        itemQuan.push(response[i].quantity);
+        tableRow += '<td>' + itemSum + '</td>';
+        tableRow += '</tr>';
+    $('#myTable tr:last').after(tableRow);
+
+}
+var itemQuan1 = itemQuan.reduce(function (a, b) {
+  return a + b;
+}, 0);
+var totalSum2 = totalSum.reduce(function (a, b) {
+  return a + b;
+}, 0);
+
+var ftableRow = '<tr id="lastOne">';
+  ftableRow += '<td>';
+  ftableRow += '<td>';
+  ftableRow += '<td>TOTAL:</td>';
+  ftableRow += '<td>'+ itemQuan1+'</td>';
+  ftableRow += '<td>'+ totalSum2.toFixed(2) +'</td>';
+  ftableRow += '</tr>';
+  $('#myTable tr:last').after(ftableRow);
+console.log(itemQuan);
+console.log(totalSum);
+
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
+var pg = require('pg');
 //uses
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
@@ -11,6 +12,15 @@ app.use(bodyParser.urlencoded({
 
 //globals
 var port = 2017;
+
+var config = {
+  database: 'omega',
+  host: 'localhost',
+  port: 5432,
+  max: 30
+};
+
+var pool = new pg.Pool(config);
 
 //spin up server
 app.listen(port, function() {
@@ -21,3 +31,55 @@ app.get('/', function(req, res) {
   console.log('in base url');
   res.sendFile(path.resolve('views/index.html'));
 }); //end base url
+
+//show customers
+app.get('/customers', function(req, res) {
+  console.log('get list');
+  pool.connect(function(err, connection, done) {
+    if (err) {
+      console.log('error');
+      done();
+      res.send(400);
+    } else {
+      console.log('connected to db');
+      var customersList = [];
+      var resultSet = connection.query('SELECT * FROM customers');
+      resultSet.on('row', function(row) {
+        customersList.push(row);
+      }); //end resultSet
+      resultSet.on('end', function() {
+        done();
+        res.send(customersList);
+      }); //end end resultSet
+    }
+  }); //done pool get
+}); //end get
+
+app.get('/orders/:id', function(req, res) {
+  console.log('get list');
+  var id = req.params.id;
+  pool.connect(function(err, connection, done) {
+    if (err) {
+      console.log('error');
+      done();
+      res.send(400);
+    } else {
+      console.log('connected to db');
+      var ordersList = [];
+      var resultSet = connection.query('SELECT orders.id, products.description, line_items.unit_price, line_items.quantity '+
+        'FROM customers '+
+        'JOIN addresses ON customers.id = addresses.customer_id' +
+	       ' JOIN orders ON addresses.id = orders.address_id '+
+	        'JOIN line_items ON orders.id = line_items.order_id'+
+	         ' JOIN products ON line_items.product_id = products.id '+
+	          'WHERE customers.id = $1',[id]);
+      resultSet.on('row', function(row) {
+        ordersList.push(row);
+      }); //end resultSet
+      resultSet.on('end', function() {
+        done();
+        res.send(ordersList);
+      }); //end end resultSet
+    }
+  }); //done pool get
+}); //end get
